a8958d5aaeaea4444065ee50a40daa74ad67ada1
Kwame Osae Afari <kayfari@MacBook-Pro.local>
2021-10-22 23:41:23 +0000

    incoperating local storage to save bookmarks

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 // Show Modal, Focus on Input
 function showModal(){
     modal.classList.add('show-modal');
@@ -34,19 +36,49 @@ function validate(nameValue, urlValue) {
     return true;
 }
 
+// Fetch bookmarks
+function fetchBookmarks(){
+    // Get bookmarks from localStorage if available
+    if(localStorage.getItem('bookmarks')){
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    }else {
+        // Create bookmarks array in localStorage
+        bookmarks =[
+            {
+                name: 'Youtube',
+                url: 'https://youtube.com',
+            },
+        ];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    console.log(bookmarks);
+}
+
 // Handle data from form
 function storeBookmark(e) {
     e.preventDefault();
     const nameValue = websiteNameEl.value;
     let urlValue = websiteUrlEl.value;
-    if(!urlValue.includes('https://')) {
+    if(!urlValue.includes('https://')){
         urlValue = `https://${urlValue}`;
     } 
     console.log(nameValue, urlValue);
     if(!validate(nameValue, urlValue)){
         return false;
     }
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
 }
 
 //Event Listener
 bookmarkForm.addEventListener('submit', storeBookmark);
+
+//onLoad, fetch bookmarks
+fetchBookmarks();
